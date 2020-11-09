fe43b10b63197335b7f930aec056d38ec1795b1a
Roman Klimchuk <gow1211@rambler.ru>
2020-11-10 03:24:54 +0600

    Create SecondTest.java

---
+++ src/test/java/SecondTest.java
@@ -0,0 +1,36 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.By;
+
+public class SecondTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,10);
+    }
+
+    @Test
+    public void secondTest() {
+        driver.get("http://localhost/litecart/admin/");
+        WebElement log = driver.findElement(By.name("username"));
+        log.sendKeys("admin");
+        WebElement pass = driver.findElement(By.name("password"));
+        pass.sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
+
